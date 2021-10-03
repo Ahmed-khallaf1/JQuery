54ec0a47879649d3673629b54af55d07f29f5412
Ahmed-khallaf1 <ahmedkhalaf329@gmail.com>
2021-10-03 13:00:16 +0200

    first commitjquery

---
+++ javascript/my.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.home').animate({
+        width: "100%",
+
+    },2000,function() {
+        $('.home').animate({
+            height: window.innerHeight 
+
+        },2000,function(){
+            $('h1').slideDown(2000,function(){
+                $('.card').eq(0).slideDown(2000,function(){
+                    $('.card').eq(1).slideDown(2000,function(){
+                        $('.card').eq(2).slideDown(2000)
+                    })
+                })
+            })
+        })
+
+    })  
+})
